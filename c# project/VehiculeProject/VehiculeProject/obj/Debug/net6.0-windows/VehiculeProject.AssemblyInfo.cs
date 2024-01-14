2463a27de494ecb69b4f4940b451c80455f64803
aziz k'h <azizkhlayfia16@gmail.com>
2024-01-14 10:10:50 +0100

    latest modif

--- c# project/VehiculeProject/VehiculeProject/obj/Debug/net6.0-windows/VehiculeProject.AssemblyInfo.cs
+++ c# project/VehiculeProject/VehiculeProject/obj/Debug/net6.0-windows/VehiculeProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VehiculeProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+addf4d99c1c11077e17ae81159816d4734aae878")]
 [assembly: System.Reflection.AssemblyProductAttribute("VehiculeProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VehiculeProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
